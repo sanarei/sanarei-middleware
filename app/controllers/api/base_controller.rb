ea80a638493d92240d61c104dabc30bd7b66ab3b
Bernard Banta <banta.bernard@gmail.com>
2025-02-25 05:15:20 +0300

    Remove basic authentication on the apis

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,9 +1,5 @@
 require './config/environment'
 
 module Api
-  class BaseController < ApplicationController
-    use Rack::Auth::Basic, 'Restricted Area' do |username, password|
-      username == ENV['APP_USERNAME'] and password == ENV['APP_PASSWORD']
-    end
-  end
+  class BaseController < ApplicationController; end
 end
