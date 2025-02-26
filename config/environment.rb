0a058561f640cde627f9e8c39ecf62ab68464ef5
Bernard Banta <banta.bernard@gmail.com>
2025-02-26 04:21:53 +0300

    Return nil if record cannot be found using Mongoid find_by query

--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,10 @@ require 'mongoid'
 Mongoid.load!('./config/database.yml', :development)
 # Mongoid::QueryCache.enabled = true
 
+# Return nil if record cannot be found using Mongoid find_by query
+Mongoid.configure do |config|
+  config.raise_not_found_error = false
+end
+
 require './app/controllers/application_controller'
 require_all 'app'
