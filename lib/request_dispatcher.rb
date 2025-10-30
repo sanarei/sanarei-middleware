880a5932fcb80a5ec452b2242d7dcceb87528e1a
Bernard Banta <banta.bernard@gmail.com>
2025-10-31 02:15:49 +0300

    Refactor RequestDispatcher domain handling
    Simplifies USSD domain input flow in RequestDispatcher. Now prompts for domain if not set, sets and confirms domain, and ends session if domain is already set. Removes unused app lookup and improves input handling.

--- lib/request_dispatcher.rb
+++ lib/request_dispatcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RequestDispatcher
   attr_reader :app, :session, :input, :phone_number, :client, :state,
               :response, :input
@@ -11,25 +13,31 @@ class RequestDispatcher
   def initialize(params)
     # Check or create a session based on sessionId
     @session = AppSession.find_or_create_by(session_id: params[:session_id])
-    @input = params[:text] # Capture the input from USSD
-
-    puts 'AppCODE'
-    puts params
-    @app = SanareiApp.find_by(app_code: params[:shortCode])
-
-    res = @app.nil? ? 'App not found' : 'App found'
-
-    @state = 'end' if @app.nil?
-    @response = "END #{res}"
+    @input = params[:text].to_s # Capture the input from USSD
   end
 
   def process_request
-    end_session = state == 'end'
+    puts "Print input: #{@input}"
+    if @session.app_domain
+      @response = 'END Domain already set!!'
+    else
+      if @input.blank?
+        @response = 'CON Enter App domain'
+      else
+        domain = @input
+        @session.update(app_domain: domain)
+        @response = "CON Domain set to #{domain}"
+      end
+    end
+
+    close_session = response.to_s.starts_with?('END')
+    response = @response.gsub('END ', '').gsub('CON ', '')
+
     {
-      shouldClose: end_session,
+      shouldClose: close_session,
       ussdMenu: response,
       responseMessage: response,
-      responseExitCode: 200,
+      responseExitCode: 200
     }
   end
 end
