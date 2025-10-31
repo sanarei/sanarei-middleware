260f4afdf9a611197af5711bd86f39d10002f780
Bernard Banta <banta.bernard@gmail.com>
2025-10-31 15:15:40 +0300

    Remove unnecessary whitespace in Sidekiq initializer
    Deleted an extra blank line in config/initializers/sidekiq.rb to improve code cleanliness.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,6 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
-  
   # Load the Sidekiq configuration
   config.logger.level = Logger::INFO
 end
